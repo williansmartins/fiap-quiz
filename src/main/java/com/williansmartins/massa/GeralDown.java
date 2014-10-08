148a8939cc85b1e66eb6285d32643bcfa12e6fdf
will <professorwillians@gmail.com>
2014-10-08 10:49:39 -0300

    mensagem de mensagem enviada, mais refactors

--- src/main/java/com/williansmartins/massa/GeralDown.java
+++ src/main/java/com/williansmartins/massa/GeralDown.java
@@ -5,18 +5,22 @@ import junit.framework.Assert;
 import org.junit.Test;
 
 import com.williansmartins.dao.JpaGenericDao;
+import com.williansmartins.dao.entity.ClienteDaoImpl;
 import com.williansmartins.dao.entity.CorretorDaoImpl;
 import com.williansmartins.dao.entity.ImovelDaoImpl;
+import com.williansmartins.entity.ClienteEntity;
 import com.williansmartins.entity.CorretorEntity;
 import com.williansmartins.entity.ImovelEntity;
 
 public class GeralDown {
 	JpaGenericDao<CorretorEntity> dao1;
 	JpaGenericDao<ImovelEntity> dao2;
+	JpaGenericDao<ClienteEntity> dao3;
 	
 	public GeralDown(){
 		dao1 = new CorretorDaoImpl();
 		dao2 = new ImovelDaoImpl();
+		dao3 = new ClienteDaoImpl();
 	}
 	
 	@Test
@@ -25,5 +29,7 @@ public class GeralDown {
 		Assert.assertEquals(true, dao1.findAll().size() == 0);
 		new ImovelMassa().removerImovel();
 		Assert.assertEquals(true, dao2.findAll().size() == 0);
+		new ClienteMassa().removerTudo();
+		Assert.assertEquals(true, dao3.findAll().size() == 0);
 	}
 }
