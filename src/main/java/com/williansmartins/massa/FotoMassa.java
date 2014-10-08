1954be5a9a3ae1325180d3e2eec78bf53c21aa86
will <professorwillians@gmail.com>
2014-10-08 16:43:17 -0300

    add dao e tests de fotos

---
+++ src/main/java/com/williansmartins/massa/FotoMassa.java
@@ -0,0 +1,34 @@
+package com.williansmartins.massa;
+
+import org.junit.Test;
+
+import com.williansmartins.dao.JpaGenericDao;
+import com.williansmartins.dao.entity.FotoDaoImpl;
+import com.williansmartins.entity.FotoEntity;
+
+public class FotoMassa {
+	
+	JpaGenericDao<FotoEntity> dao;
+	
+	public FotoMassa(){
+		dao = new FotoDaoImpl();
+	}
+
+	@Test
+	public void inserirImovel() {
+		dao.insert(getEntityData());
+	}
+	
+	public FotoEntity getEntityData( ) {
+		FotoEntity entity = new FotoEntity();
+		entity.setGrande("grande");
+		entity.setThumb("thumb");
+		return entity;
+	}
+	
+	public void removerImovel() {
+		for (FotoEntity o : dao.findAll()) {
+			dao.delete( o.getId() );
+		}
+	}
+}
